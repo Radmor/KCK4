2b4e51b1dc87502aa18866dc6ea38e6cb925cff9
sdsdsfd <example@example.com>
2016-11-18 09:49:43 +0100

    Afterclass commit

--- catan.py
+++ catan.py
@@ -12,25 +12,30 @@ for filename in os.listdir("input"):
 
 out = []
 for i in range(0, len(input)):
-    r = np.zeros((len(input[i]), len(input[i][0])))
+    r = np.zeros_like(input_grey[i])
     g = np.zeros((len(input[i]), len(input[i][0])))
     b = np.zeros((len(input[i]), len(input[i][0])))
+    threshb = np.zeros((len(input[i]), len(input[i][0])))
     for j in range(0, len(input[i])):
         for k in range(0, len(input[i][j])):
             r[j][k] = input[i][j][k][0]
             g[j][k] = input[i][j][k][1]
             b[j][k] = input[i][j][k][2]
-    sobel = filters.sobel(b)
-    canny = feature.canny(b, sigma=150)
-    out.append([r, g, b, sobel, canny])
+            if b[j][k] > r[j][k]*4/5:
+                threshb[j][k] = 1
+            else:
+                threshb[j][k] = 0
+    sobel = filters.sobel(threshb)
+    canny = feature.canny(threshb, sigma=10)
+    out.append([threshb, sobel, canny])
 
 
 for i in range(0, len(input)):
-    plt.subplot(2, 3, 1)
+    plt.subplot(2, 2, 1)
     plt.imshow(input[i])
 
     for nn, img in enumerate(out[i]):
-        plt.subplot(2, 3, nn+2)
+        plt.subplot(2, 2, nn+2)
         plt.imshow(img)
         plt.gray()
 
