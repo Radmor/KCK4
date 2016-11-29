1a0ed1f9d86a537ee1d5046d5ce0218faa53f88b
Jan Bajer <jan.bajer@gmail.com>
2016-11-29 22:16:10 +0100

    Migrated to opencv: finding corners automated, but need to add color reading again

--- catan.py
+++ catan.py
@@ -1,8 +1,11 @@
-from skimage import data, filters, morphology, feature, draw, measure
+from skimage import data, filters, morphology, feature, draw, measure, color
 from matplotlib import pyplot as plt
+plt.switch_backend('Qt4Agg')
 import numpy as np
 import os
 import copy
+from scipy.ndimage.morphology import binary_fill_holes
+import cv2
 
 INPUT_DIR_PATH = 'input'
 
@@ -23,26 +26,97 @@ def list_file_paths(dir_path):
 
 
 def get_input_data(file_paths):
-    return [data.imread(file) for file in file_paths], [data.imread(file, as_grey=True) for file in file_paths]
+    # return [cv2.imread(file, 1) for file in file_paths], [cv2.imread(file, 0) for file in file_paths]
+
+
+    imgs = [cv2.imread(file, 1) for file in file_paths]
+    rs = [500.0 / img.shape[1] for img in imgs]
+    dims = [(500, int(img.shape[0] * r)) for img, r in zip(imgs, rs)]
+    return [cv2.resize(image, dim, interpolation=cv2.INTER_AREA) for image, dim in zip(imgs, dims)], [cv2.resize(
+        cv2.imread(file, 0), dim, interpolation=cv2.INTER_AREA) for file, dim in zip(file_paths, dims)]
 
 
 def perform_image_computations(input, input_grey):
     out = []
     for input_item, input_grey_item in zip(input, input_grey):
-        zeros = np.zeros_like(input_grey_item)
-        r, g, b = zeros, zeros, zeros
+        r = np.zeros_like(input_grey_item)
         g = np.zeros_like(input_grey_item)
         b = np.zeros_like(input_grey_item)
         threshb = np.zeros_like(input_grey_item)
+        threshb2 = np.zeros_like(input_grey_item)
+        hsv = color.rgb2hsv(input_item)
+        bmax = 0
 
+        # for j, input_item_row in enumerate(input_item):
+        #     for k, input_item_cell in enumerate(input_item_row):
+                # b[j][k], g[j][k], r[j][k] = input_item_cell
+                # threshb[j][k] = b[j][k] > r[j][k]*THRESHOLD_VALUE
+
+                # r[j][k], g[j][k], b[j][k] = input_item_cell
+                # if b[j][k] > bmax:
+                #     bmax = b[j][k]
+
+                # if 120/360 <= input_item_cell[0] <= 340/360 and input_item_cell[1] > 0.1:
+                #     threshb[j][k] = 1.0
+                #     # print(input_item_cell)
+                # else:
+                #     threshb[j][k] = 0.0
+        # print(bmax)
         for j, input_item_row in enumerate(input_item):
             for k, input_item_cell in enumerate(input_item_row):
-                r[j][k], g[j][k], b[j][k] = input_item_cell
-                threshb[j][k] = b[j][k] > r[j][k]*THRESHOLD_VALUE
+                # print(input_grey_item[j][k])
+                b[j][k], g[j][k], r[j][k] = input_item_cell
+                threshb[j][k] = b[j][k] > r[j][k]*THRESHOLD_VALUE and 200 > input_grey_item[j][k] > 50
+        # filled = binary_fill_holes(threshb)
+
+        # filling holes
+        # Copy the thresholded image.
+        im_floodfill = threshb.copy()
+
+        # Mask used to flood filling.
+        # Notice the size needs to be 2 pixels than the image.
+        h, w = threshb.shape[:2]
+        mask = np.zeros((h + 2, w + 2), np.uint8)
+
+        # Floodfill from point (0, 0)
+        cv2.floodFill(im_floodfill, mask, (0, 0), 255);
+
+        # Invert floodfilled image
+        im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+        # Combine the two images to get the foreground.
+        hex = threshb | im_floodfill_inv
+
+
+
+        # for i in range(0, 20):
+        #     filled = morphology.binary_closing(filled)
+        #     filled = morphology.binary_opening(filled)
+
+        # blurred = cv2.GaussianBlur(filled, (5, 5), 0)
+
+        # c = cv2.findContours(filled.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # peri = cv2.arcLength(c, True)
+        # approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+        # print(approx)
+        # print(threshb)
+        # print(input_grey_item)
+
+        #
+        # cv2.imshow('img', input_item)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # cv2.imshow('img', hexclr)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        # # plt.imshow(filled)
+        # plt.gray()
+        # plt.show()
         sobel = filters.sobel(threshb)
         canny = feature.canny(threshb, sigma=CANNY_SIGMA_VALUE)
         # out.append([threshb, sobel, canny])
-        out.append([threshb, sobel])
+        out.append(hex)
 
     return out
 
@@ -53,36 +127,47 @@ def get_hex_coords(vexlist):
     if len(vexlist) != 6:
         return -1
 
+    vexlist = [el[0] for el in vexlist]
+
     # sort vexlist
-    vexx = [coord[0] for coord in vexlist]
-    sortx = sorted(vexx)
+    # vexx = [coord[0] for coord in vexlist]
+    # sortx = sorted(vexx)
+    #
+    # nodups = len(vexx) == len(set(vexx))
+    # sortvex = []
+    # if nodups:
+    #     for x in sortx:
+    #         el = [item for item in vexlist if item[0] == x]
+    #         vexlist.remove(el[0])
+    #         sortvex.append(el[0])
+    #
+    # else:
+    #     vexy = [coord[1] for coord in vexlist]
+    #     sorty = sorted(vexy)
+    #     for x in sortx:
+    #         ellist = []
+    #         for y in sorty:
+    #             el = [item for item in vexlist if item[0] == x and item[1] == y]
+    #             if len(el) > 0:
+    #                 ellist.append(el[0])
+    #         vexlist.remove(ellist[0])
+    #         sortvex.append(ellist[0])
+    #
+    # botleft = sortvex[0]
+    # botright = sortvex[1]
+    # left = sortvex[2]
+    # right = sortvex[3]
+    # topleft = sortvex[4]
+    # topright = sortvex[5]
 
-    nodups = len(vexx) == len(set(vexx))
-    sortvex = []
-    if nodups:
-        for x in sortx:
-            el = [item for item in vexlist if item[0] == x]
-            vexlist.remove(el[0])
-            sortvex.append(el[0])
+    # no sorting, since opencv function gives vertices in order around the contour
 
-    else:
-        vexy = [coord[1] for coord in vexlist]
-        sorty = sorted(vexy)
-        for x in sortx:
-            ellist = []
-            for y in sorty:
-                el = [item for item in vexlist if item[0] == x and item[1] == y]
-                if len(el) > 0:
-                    ellist.append(el[0])
-            vexlist.remove(ellist[0])
-            sortvex.append(ellist[0])
-
-    botleft = sortvex[0]
-    botright = sortvex[1]
-    left = sortvex[2]
-    right = sortvex[3]
-    topleft = sortvex[4]
-    topright = sortvex[5]
+    left = vexlist[0]
+    topleft = vexlist[1]
+    topright = vexlist[2]
+    right = vexlist[3]
+    botright = vexlist[4]
+    botleft = vexlist[5]
 
     left_to_right = (right[0] - left[0], right[1] - left[1])
     botleft_to_topright = (topright[0] - botleft[0], topright[1] - botleft[1])
@@ -148,6 +233,25 @@ def compute_hex_colors(hexinfo, input, input_grey):
     return [get_hex_colors(hex, image, image_grey) for hex, image, image_grey in zip(hexinfo, input, input_grey)]
 
 
+def get_corners_contours_opencv(input):
+    vertices = []
+    contours = []
+    for input_item in input:
+        # getting contours
+        cnts = cv2.findContours(input_item.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in cnts[1]:
+            # cv2.drawContours(input_item, [c], -1, (0, 255, 0), 2)
+            # hexclr = cv2.cvtColor(hex, cv2.COLOR_GRAY2RGB)
+            # cv2.drawContours(hexclr, [c], -1, (0, 255, 0), 2)
+            peri = cv2.arcLength(c, True)
+            corners = cv2.approxPolyDP(c, 0.04 * peri, True)
+
+        vertices.append(corners.tolist())
+        contours.append(cnts)
+    return vertices, contours
+
+
 def get_gameboard_corners(input):
     for input_item, second in input:
         # coords = feature.corner_peaks(feature.corner_harris(input_item,k=0.24), min_distance=50,)
@@ -175,6 +279,7 @@ def get_gameboard_corners(input):
         # ax.plot(coords_subpix[:, 1], coords_subpix[:, 0], '+r', markersize=15)
         ax.plot(coords[:, 1], coords[:, 0], '+r', markersize=15)
         plt.show()
+
 
 def get_gameboard_corners(input):
     for input_item, second in input:
@@ -207,45 +312,61 @@ def get_gameboard_corners(input):
         return feature.corner_peaks(feature.corner_moravec(input_item,), min_distance=50,)
 
 
+def plot_images(input, filled, hexinfo, vertices, contours):
+    for input_item, filled_item, hexinfo_item, vertex_item, contour_item in zip(input, filled, hexinfo, vertices, contours):
+        image = input_item.copy()
+        for c in contour_item[1]:
+            cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+        for v in hexinfo_item[0]:
+            vmod = tuple([int(v[0]), int(v[1])])
+            cv2.circle(image, vmod, 10, (255, 255, 255))
+        # for i, v in enumerate(vertex_item):
+        #     cv2.circle(image, tuple(v[0]), 10, (0, 0, 100 + 30*i), 5)
+        cv2.imshow('img', image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-def plot_images(input, out, hexinfo, colors, corners):
-    for input_item, out_item, hexinfo_item, colors_item,corner_item in zip(input, out, hexinfo, colors, corners):
-        x_size, y_size = PLOT_SHAPE
-        ax = plt.subplot(x_size, y_size, 1)
-        plt.imshow(input_item)
 
-        for el in hexinfo_item[0]:
-            circle = plt.Circle((el[0], el[1]), radius=hexinfo_item[1], alpha=0.4, color='white')
-            ax.add_artist(circle)
+        # x_size, y_size = PLOT_SHAPE
+        # ax = plt.subplot(x_size, y_size, 1)
+        # plt.imshow(input_item)
 
-        ax = plt.subplot(x_size, y_size, 2)
-        plt.imshow(out_item[0])
-        for el, col in zip(hexinfo_item[0], colors_item):
-            circle = plt.Circle((el[0], el[1]), radius=hexinfo_item[1], color=col)
-            ax.add_artist(circle)
+        # for el in hexinfo_item[0]:
+        #     circle = plt.Circle((el[0], el[1]), radius=hexinfo_item[1], alpha=0.4, color='white')
+        #     ax.add_artist(circle)
 
-        ax.plot(corners[:, 1], corners[:, 0], '+r', markersize=15)
+        # ax = plt.subplot(x_size, y_size, 2)
+        # plt.imshow(out_item[0])
+        # for el, col in zip(hexinfo_item[0], colors_item):
+        #     circle = plt.Circle((el[0], el[1]), radius=hexinfo_item[1], color=col)
+        #     ax.add_artist(circle)
 
-        for index, img in enumerate(out_item):
+        # ax.plot(corners[:, 1], corners[:, 0], '+r', markersize=15)
 
-            ax = plt.subplot(x_size, y_size, index+3)
-            ax.plot(corners[:, 1], corners[:, 0], '+r', markersize=15)
-            plt.imshow(img)
-            plt.gray()
-
-        plt.show()
+        # for index, img in enumerate(out_item):
+        #
+        #     ax = plt.subplot(x_size, y_size, index+3)
+        #     # ax.plot(corners[:, 1], corners[:, 0], '+r', markersize=15)
+        #     plt.imshow(img)
+        #     plt.gray()
+        #
+        # plt.show()
 
 file_paths = list_file_paths(INPUT_DIR_PATH)
 input, input_grey = get_input_data(file_paths)
-hexinfo = compute_vertices(vlist)
-colors = compute_hex_colors(hexinfo, input, input_grey)
+filled = perform_image_computations(input, input_grey)
+vertices, contours = get_corners_contours_opencv(filled)
+# print(vertices)
+hexinfo = compute_vertices(vertices)
+# print(vertices)
+# colors = compute_hex_colors(hexinfo, input, input_grey)
 
 
-out = perform_image_computations(input, input_grey)
 
-corners = get_gameboard_corners(out)
 
-plot_images(input, out, hexinfo, colors,corners)
+# corners = get_gameboard_corners(out)
+
+plot_images(input, filled, hexinfo, vertices, contours)
 
 
 
